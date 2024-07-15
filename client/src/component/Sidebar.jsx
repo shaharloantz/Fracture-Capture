7364f9a15df5b0ab4e5ce0261463ff3a7c31b401
Tal <zigelnik@cs.colman.ac.il>
2024-07-15 16:37:06 +0300

    dashboard page

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -12,6 +12,14 @@ export default function Sidebar() {
                 Menu
             </button>
             <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <Link to="/about" onClick={toggleSidebar} className="sidebar-link">
+                    <img src="/src/assets/images/info-icon.png" alt="About" className="sidebar-icon"/>
+                    <span>About</span>
+                </Link>
+                <Link to="/dashboard" onClick={toggleSidebar} className="sidebar-link">
+                    <img src="/src/assets/images/dashboard-icon.png" alt="Dashboard" className="sidebar-icon"/>
+                    <span>Dashboard</span>
+                </Link>
                 <Link to="/profile" onClick={toggleSidebar} className="sidebar-link">
                     <img src="/src/assets/images/myprofile-icon.png" alt="My Profile" className="sidebar-icon"/>
                     <span>My Profile</span>
@@ -19,10 +27,6 @@ export default function Sidebar() {
                 <Link to="/qa" onClick={toggleSidebar} className="sidebar-link">
                     <img src="src/assets/images/qa-icon.png" alt="Q&A" className="sidebar-icon"/>
                     <span>Q&A</span>
-                </Link>
-                <Link to="/about" onClick={toggleSidebar} className="sidebar-link">
-                    <img src="/src/assets/images/info-icon.png" alt="About" className="sidebar-icon"/>
-                    <span>About</span>
                 </Link>
             </div>
         </>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,9 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-export default function Profile() {
+
+const Dashboard = () => {
+
     const [profile, setProfile] = useState(null);
     const navigate = useNavigate();
 
@@ -11,11 +13,10 @@ export default function Profile() {
         axios.get('/user/profile', { withCredentials: true })
             .then(response => {
                 setProfile(response.data);
-              //  toast.success('Profile loaded successfully!');
+                //toast.success('Profile loaded successfully!');
             })
             .catch(error => {
                 console.error('Error fetching profile:', error.response ? error.response.data : error.message);
-                //toast.error('Error fetching profile. Please log in.');
                 navigate('/login');
             });
     }, [navigate]);
@@ -23,12 +24,7 @@ export default function Profile() {
     if (!profile) {
         return <div>Loading...</div>;
     }
-
-    return (
-        <div className="profile-container">
-            <p><strong>{profile.message}</strong> </p>
-            <p><strong>Name:</strong> {profile.name}</p>
-            <p><strong>Email:</strong> {profile.email}</p>
-        </div>
-    );
 }
+
+export default Dashboard
+
