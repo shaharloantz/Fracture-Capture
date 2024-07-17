4cbce3929d978a9461dde5b7cfe1f96d8a9eb6d0
Tal <zigelnik@cs.colman.ac.il>
2024-07-17 11:35:44 +0300

    added multer pkg

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const requireAuth = require('../middleware/authMiddleware');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const router = express.Router();
 
