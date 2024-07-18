f6e6601c83f5b9af6cdffa1ea1b2d231d6bbaadc
Tal <zigelnik@cs.colman.ac.il>
2024-07-18 11:29:56 +0300

    fixed flow for uploading and patient creating

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     const navigate = useNavigate();
 
     const handlePlayClick = () => {
-        navigate('/upload'); // Navigate to upload page
+        navigate('/dashboard'); // Navigate to upload page
     };
 
     return (
