c51d085e315aed6922f83559d39ca201760af1b9
Tal <zigelnik@cs.colman.ac.il>
2024-08-09 11:15:00 +0300

    Revert "Mainly CSS"
    This reverts commit e9b67db785790f23beef50b58a26afad22525553.

--- client/src/component/profile/PatientList.jsx
+++ client/src/component/profile/PatientList.jsx
@@ -7,18 +7,18 @@ const PatientList = ({ patients, fetchPatientUploads, handleEditPatientClick, ha
                 <div key={patient._id} className="patient-folder">
                     <img src="/src/assets/images/folderNEW.png" alt="Patient Folder" className="folder-icon" onClick={() => fetchPatientUploads(patient._id)} />
                     <div className="patient-info">
-                    <p><strong>Patient Name:</strong><br /> <span className="patient-detail">{patient.name}</span></p>
-                    <p><strong>ID:</strong> {patient.idNumber}</p>
+                        <p><strong>Patient Name:</strong> {patient.name}</p>
+                        <p><strong>ID:</strong> {patient.idNumber}</p>
                     </div>
                     <div className="icon-container">
                         <img 
-                            src="src/assets/images/pen.png" 
+                            src="src/assets/images/edit-text.png" 
                             alt="Edit" 
                             className="edit-icon" 
                             onClick={(e) => handleEditPatientClick(patient, e)}
                         />
                         <img 
-                            src="src/assets/images/delete.png" 
+                            src="src/assets/images/bin.png" 
                             alt="Delete" 
                             className="delete-icon" 
                             onClick={(e) => handleDeletePatientClick(patient._id, e)}
