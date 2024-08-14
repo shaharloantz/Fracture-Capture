3f2916a1ed3e317c889beeb03b553129f00c11b5
Tal <zigelnik@cs.colman.ac.il>
2024-08-14 13:06:13 +0300

    edit user for admin privilages

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -5,6 +5,8 @@ import '../styles/AdminPanel.css'; // Adjust the path if necessary
 const AdminPanel = () => {
     const [users, setUsers] = useState([]);
     const [sortOption, setSortOption] = useState('isAdmin'); // Default sort by isAdmin
+    const [editingUserId, setEditingUserId] = useState(null); // Track the user being edited
+    const [editedUserData, setEditedUserData] = useState({ name: '', email: '' });
 
     useEffect(() => {
         axios.get('/user/all-users', { withCredentials: true })
@@ -18,6 +20,37 @@ const AdminPanel = () => {
 
     const handleSortChange = (e) => {
         setSortOption(e.target.value);
+    };
+
+    const handleEditClick = (user) => {
+        if (editingUserId === user._id) {
+            // If the user is already being edited, close the editing mode
+            setEditingUserId(null);
+        } else {
+            // Otherwise, open the editing mode for this user
+            setEditingUserId(user._id);
+            setEditedUserData({ name: user.name, email: user.email });
+        }
+    };
+    
+    const handleCancelClick = () => {
+        setEditingUserId(null); // Close the editing mode
+    };
+    
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedUserData({ ...editedUserData, [name]: value });
+    };
+
+    const handleSaveClick = async () => {
+        try {
+            await axios.put(`/user/update/${editingUserId}`, editedUserData, { withCredentials: true });
+            setUsers(users.map(user => user._id === editingUserId ? { ...user, ...editedUserData } : user));
+            setEditingUserId(null); // Close the edit mode
+        } catch (error) {
+            console.error('Error updating user:', error.response ? error.response.data : error.message);
+        }
     };
 
     const sortedUsers = [...users].sort((a, b) => {
@@ -37,7 +70,7 @@ const AdminPanel = () => {
 
     return (
         <div className="admin-panel">
-            <h1>Admin Panel</h1>
+            <h2>Admin Panel</h2>
             <div className="sort-options">
                 <label htmlFor="sort">Sort by: </label>
                 <select id="sort" value={sortOption} onChange={handleSortChange}>
@@ -59,12 +92,59 @@ const AdminPanel = () => {
                     </thead>
                     <tbody>
                         {sortedUsers.map(user => (
-                            <tr key={user._id}>
-                                <td>{user.isAdmin ? 'Admin' : 'Regular'}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.numberOfPatients}</td>
-                            </tr>
+                            <>
+                                <tr key={user._id}>
+                                    <td>{user.isAdmin ? 'Admin' : 'Regular'}</td>
+                                    <td>{user.name}</td>
+                                    <td>
+                                        {user.email} 
+                                        {!user.isAdmin && (
+                                        <img 
+                                            src="/src/assets/images/pen.png" 
+                                            alt="Edit" 
+                                            className="admin-edit" 
+                                            onClick={() => handleEditClick(user)} 
+                                        />
+                                        )}
+                                    </td>
+                                    <td>{user.numberOfPatients}</td>
+                                </tr>
+                                                 {editingUserId === user._id && (
+                                                <tr key={`edit-${user._id}`} className="edit-row">
+                                                    <td colSpan="4">
+                                                        <div className="edit-form">
+                                                            <div className="form-row">
+                                                                <label>
+                                                                    Name: 
+                                                                    <input 
+                                                                        type="text" 
+                                                                        name="name" 
+                                                                        value={editedUserData.name} 
+                                                                        onChange={handleInputChange} 
+                                                                        style={{color:'black'}}
+                                                                    />
+                                                                </label>
+                                                                <label>
+                                                                    Email: 
+                                                                    <input 
+                                                                        type="email" 
+                                                                        name="email" 
+                                                                        value={editedUserData.email} 
+                                                                        onChange={handleInputChange} 
+                                                                        style={{color:'black'}}
+                                                                    />
+                                                                </label>
+                                                            </div>
+                                                            <div className="button-row">
+                                                                <button onClick={handleSaveClick}>Save</button>
+                                                                <button onClick={handleCancelClick}>Cancel</button>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )}
+
+                            </>
                         ))}
                     </tbody>
                 </table>
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -104,5 +104,29 @@ router.get('/all-users', requireAuth, async (req, res) => {
     }
 });
 
+// edit user by admin
+router.put('/update/:userId', requireAuth, async (req, res) => {
+    const { userId } = req.params;
+    const { name, email } = req.body;
 
+    try {
+        // Find the user by ID
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Update the user's name and email
+        user.name = name;
+        user.email = email;
+
+        // Save the updated user
+        await user.save();
+
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 module.exports = router;
