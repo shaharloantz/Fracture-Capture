0b6967e75973e8c3fc0afb93d5ff52d0e0016c8a
Tal <zigelnik@cs.colman.ac.il>
2024-08-21 17:31:44 +0300

    Update Footer.jsx
    space

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <p>&copy; 2024 FractureCapture. All rights reserved.</p>
         <p>
           <Link onClick={()=>scroll.scrollToTop()}to="/terms-of-service">Terms of Service</Link> | 
-          <Link onClick={()=>scroll.scrollToTop()} to="/privacy-policy">Privacy Policy</Link> | 
-          <Link onClick={()=>scroll.scrollToTop()} to="/Contact-Us">Contact Us</Link>
+          <Link onClick={()=>scroll.scrollToTop()} to="/privacy-policy"> Privacy Policy</Link> |  
+          <Link onClick={()=>scroll.scrollToTop()} to="/Contact-Us"> Contact Us</Link>
         </p>
         <p> ט.ל.ח</p>
       </div>
