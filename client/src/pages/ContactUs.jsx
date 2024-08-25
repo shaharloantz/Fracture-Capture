4f13e8367f1c321f26579fbb993ba481033ec000
DanaHa-24 <dana24997@gmail.com>
2024-08-26 00:15:10 +0300

    Alignment of contact-us page design

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 import '../styles/ContactUs.css';
 
 const ContactUs = () => {
@@ -24,46 +25,49 @@ const ContactUs = () => {
                 reply_to: formData.email,
             });
             console.log('Email sent successfully:', response.data);
-            alert('Email sent successfully!');
+            toast.success('Email sent successfully!');
+            setFormData({
+                name: '',
+                email: '',
+                message: '',
+            });
         } catch (error) {
-            console.error('Error sending email:', error);
-            alert('Failed to send email. Please try again.');
+            console.error('Error sending email:', error.response ? error.response.data : error.message);
+            toast.error(error.response ? error.response.data.error : 'Failed to send email. Please try again.');
         }
     };
-    
-    
 
     return (
         <div className="contact-us">
             <h2>Contact Us</h2>
             <form onSubmit={handleSubmit} className="contact-form">
                 <div className="form-group">
-                    <label htmlFor="name">Name</label>
                     <input
                         type="text"
                         id="name"
                         name="name"
+                        placeholder="Enter Your Name"
                         value={formData.name}
                         onChange={handleChange}
                         required
                     />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email</label>
                     <input
                         type="email"
                         id="email"
                         name="email"
+                        placeholder="Enter Your Email"
                         value={formData.email}
                         onChange={handleChange}
                         required
                     />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="message">Message</label>
                     <textarea
                         id="message"
                         name="message"
+                        placeholder="Enter Your Message"
                         value={formData.message}
                         onChange={handleChange}
                         required
