ff5ac2415c9a0d6de09dbacccffe6bad07e31ef3
Jonathan Cwengel <jonathancwengel@gmail.com>
2024-08-28 16:43:08 +0300

    Folders responsiveness
    Trying to fix the folders responsiveness - instead of showing in a vertical line, make them horizontal.

--- client/src/component/patients/PatientUploads.jsx
+++ client/src/component/patients/PatientUploads.jsx
@@ -10,26 +10,28 @@ const PatientUploads = ({ patientUploads, handleUploadClick, handleDeleteUploadC
             onClick={handleBackClick} 
             style={{ margin: '0 auto', marginBottom: '20px' }}
         />
-        <div className="upload-folders">
-            {patientUploads.length > 0 ? patientUploads.map(upload => (
-                <div key={upload._id} className="upload-folder">
-                    <img src="/src/assets/images/folderNEW.png" alt="Upload Folder" className="folder-icon" onClick={() => handleUploadClick(upload)} />
-                    <div className="icon-container">
-                    <div className="upload-info">
-                        <p><strong>{formatDate(upload.dateUploaded)}</strong> </p>
-                        <p><strong> {upload.bodyPart}</strong></p>
+        <div className="upload-folders-container">
+            <div className="upload-folders">
+                {patientUploads.length > 0 ? patientUploads.map(upload => (
+                    <div key={upload._id} className="upload-folder">
+                        <img src="/src/assets/images/folderNEW.png" alt="Upload Folder" className="folder-icon" onClick={() => handleUploadClick(upload)} />
+                        <div className="icon-container">
+                            <div className="upload-info">
+                                <p><strong>{formatDate(upload.dateUploaded)}</strong></p>
+                                <p><strong>{upload.bodyPart}</strong></p>
+                            </div>
+                            <img 
+                                src="src/assets/images/bin.png" 
+                                alt="Delete" 
+                                className="delete-icon" 
+                                onClick={(e) => handleDeleteUploadClick(upload._id, e)}
+                            />
+                        </div>
                     </div>
-                        <img 
-                            src="src/assets/images/bin.png" 
-                            alt="Delete" 
-                            className="delete-icon" 
-                            onClick={(e) => handleDeleteUploadClick(upload._id, e)}
-                        />
-                    </div>
-                </div>
-            )) : (
-                <p>No uploads found for this patient.</p>
-            )}
+                )) : (
+                    <p>No uploads found for this patient.</p>
+                )}
+            </div>
         </div>
     </>
 );
