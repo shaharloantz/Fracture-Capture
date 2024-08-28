047fdfbddd50c78eb2534b1cfc24131a5c74f063
DanaHa-24 <dana24997@gmail.com>
2024-08-28 19:35:16 +0300

    Add responsive to UploadDetails

--- client/src/component/patients/UploadDetails.jsx
+++ client/src/component/patients/UploadDetails.jsx
@@ -1,34 +1,3 @@
-/**
- * UploadDetails Component
- 
- * This component is responsible for displaying the details of a selected upload, including patient information,
- * the associated doctor, the processed image, and predictions made by the system. The user can download the details 
- * as a PDF or share them via email. The component also provides functionality for sharing the upload with another 
- * doctor via email.
- *
- * Key Features:
- * - Displays detailed information about the upload and the patient.
- * - Allows downloading the details as a PDF.
- * - Enables sending the details via email.
- * - Provides the ability to share the upload with another doctor.
- *
- * Props:
- * - selectedUpload: The upload object containing details of the selected upload.
- * - handleBackClick: A function to handle the event when the user clicks the "Back" button.
- * - patient: The patient object containing the patient's information.
- *
- * State:
- * - imageLoaded: Tracks whether the processed image has been loaded.
- * - createdByUser: Stores details of the user who created the upload.
- * - email: Stores the email address for sending the upload details.
- * - shareEmail: Stores the email address for sharing the upload with another doctor.
- * - message: Stores messages to display to the user (e.g., success or error messages).
- * - showEmailInput: Toggles the visibility of the email input field for sending details.
- * - isSending: Tracks whether the email is being sent.
- *
- * The component uses Axios for API calls and includes various functions to handle fetching user data, 
- * downloading PDFs, sending emails, and sharing uploads.
- */
 import React, { useState, useEffect } from 'react';
 import downloadIcon from '../../assets/images/download-file-icon.png';
 import sendEmailIcon from '../../assets/images/send-email-icon.png';
@@ -36,6 +5,7 @@ import shareIcon from '../../assets/images/share-folder-icon.png';
 import axios from 'axios';
 import { createPDF, sendEmail } from '../../utils/pdfUtils';
 import { toast } from 'react-hot-toast';
+import '../../styles/UploadDetails.css';
 
 const UploadDetails = ({ selectedUpload, handleBackClick, patient }) => {
     const [imageLoaded, setImageLoaded] = useState(false);
@@ -75,7 +45,7 @@ const UploadDetails = ({ selectedUpload, handleBackClick, patient }) => {
     };
 
     const downloadPDF = async () => {
-        const pdf = await createPDF(selectedUpload, patient,createdByUser , imageLoaded);
+        const pdf = await createPDF(selectedUpload, patient, createdByUser, imageLoaded);
         if (pdf) {
             pdf.save(`upload_details_${selectedUpload.patientName || 'unknown'}.pdf`);
         }
@@ -103,13 +73,14 @@ const UploadDetails = ({ selectedUpload, handleBackClick, patient }) => {
 
     return (
         <>
-            <img 
-                src="/src/assets/images/undo.png" 
-                alt="Back to Patients" 
-                className="back-button-icon" 
-                onClick={handleBackClick} 
-                style={{ margin: '0 auto', marginBottom: '20px' }}
-            />
+            <div className="back-button-container">
+                <img 
+                    src="/src/assets/images/undo.png" 
+                    alt="Back to Patients" 
+                    className="back-button-icon" 
+                    onClick={handleBackClick} 
+                />
+            </div>
             <div className="upload-details">
                 <div id="pdf-content">
                     <p><strong>Patient Name:</strong> <span>{selectedUpload.patientName || 'N/A'}</span></p>
@@ -129,55 +100,57 @@ const UploadDetails = ({ selectedUpload, handleBackClick, patient }) => {
                         alt="Processed Upload" 
                         onLoad={handleImageLoad} 
                         className="processed-image" 
-                        style={{ marginTop: '15px' }} 
                     />
                 </div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px', gap:'20vh' }}>
+                <div className="action-icons">
                     <img 
                         src={downloadIcon} 
                         alt="Download as PDF" 
                         onClick={downloadPDF} 
-                        style={{ cursor: 'pointer', width: '60px', marginRight: '20px' }}
+                        className="icon"
                     />
                     <img 
                         src={shareIcon}
                         alt="Share to another Dr." 
                         onClick={() => setShowShareInput(!showShareInput)}
-                        style={{ cursor: 'pointer', width: '60px' }}
+                        className="icon"
                     />
                     <img 
                         src={sendEmailIcon}
                         alt="Send as Email" 
                         onClick={() => setShowEmailInput(!showEmailInput)}
-                        style={{ cursor: 'pointer', width: '60px' }}
+                        className="icon"
                     />
                 </div>
 
                 {showEmailInput && (
-                    <form onSubmit={(e) => { 
-                        e.preventDefault(); 
-                        if (!email) {
-                            toast.error('Please enter a valid email address.');
-                            return;
-                        }
-                        sendEmail(selectedUpload, email, imageLoaded, setIsSending, setShowEmailInput, setEmail, patient, createdByUser); 
-                    }} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+                    <form 
+                        onSubmit={(e) => { 
+                            e.preventDefault(); 
+                            if (!email) {
+                                toast.error('Please enter a valid email address.');
+                                return;
+                            }
+                            sendEmail(selectedUpload, email, imageLoaded, setIsSending, setShowEmailInput, setEmail, patient, createdByUser); 
+                        }} 
+                        className="email-form"
+                    >
                         <input
                             type="email"
                             value={email}
                             onChange={handleEmailChange}
                             placeholder="Enter recipient email"
-                            style={{ padding: '10px', width: '40%', marginBottom: '10px', color: 'black' }}
+                            className="email-input"
                             disabled={isSending}
                         />
-                        <button type="submit" style={{ padding: '10px 20px', cursor: 'pointer' }} disabled={isSending}>
+                        <button type="submit" className="form-button" disabled={isSending}>
                             {isSending ? 'Sending...' : 'Send'}
                         </button>
                     </form>
                 )}
 
                 {showShareInput && (
-                    <form onSubmit={handleShareSubmit} style={{ marginTop: '20px' }}>
+                    <form onSubmit={handleShareSubmit} className="email-form">
                         <label>
                             <p>Share with another doctor:</p>
                             <input
@@ -186,13 +159,13 @@ const UploadDetails = ({ selectedUpload, handleBackClick, patient }) => {
                                 placeholder="Enter doctor's email"
                                 onChange={handleShareEmailChange}
                                 required
-                                style={{ padding: '10px 20px', width: '40%' }}
+                                className="email-input"
                             />
                         </label>
-                        <button type="submit" style={{ padding: '10px 20px', cursor: 'pointer' }}>Share</button>
+                        <button type="submit" className="form-button">Share</button>
                     </form>
                 )}
-                {message && <p>{message}</p>}
+                {message && <p className="message">{message}</p>}
             </div>
         </>
     );
