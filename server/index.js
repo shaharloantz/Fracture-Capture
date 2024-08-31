c84e6c65372b324d9d833b53f3b249452e18c46f
Shahar Loantz <shaharloantz1@gmail.com>
2024-08-31 18:00:14 +0300

    add cors origin to support the differents options

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','http://127.0.0.1:5173'],
 }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
